b9ed8240fed52698681fb4620c7f582129631d51
Ivan Santana <ivansantana1904@gmail.com>
2024-10-15 13:27:28 -0400

    UpdateCalPanel

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+function toggleMCB() {
+    const mcbCheckbox = document.getElementById("mcb").checked;
+    document.getElementById("mcbOptions").style.display = mcbCheckbox ? 'block' : 'none';
+}
+
+
 function calcularAltura() {
     const breakerA1 = parseInt(document.getElementById("breakerA1").value) || 0;
     const breakerA2 = parseInt(document.getElementById("breakerA2").value) || 0;
     const breakerA3 = parseInt(document.getElementById("breakerA3").value) || 0;
     const amperaje = parseInt(document.getElementById("amperajeInput").value);
     const incluirMLO = document.getElementById("mloCheck").checked;
+    const incluirMCB = document.getElementById("mcb").checked;
+    let mcbTamaño = 0;
 
     // Verificar si no se seleccionó ningún breaker
     if (breakerA1 === 0 && breakerA2 === 0 && breakerA3 === 0) {
@@ -16,7 +24,7 @@ function calcularAltura() {
     const alturaBreakersA2 = Math.ceil(breakerA2 / 2) * 4.125;   // Cada 2 breakers A2 = 4.125 pulgadas
     const alturaBreakersA3 = Math.ceil(breakerA3 / 2) * 5.5;     // Cada 2 breakers A3 = 5.5 pulgadas
 
-    const alturaTotalBreakers = alturaBreakersA1 + alturaBreakersA2 + alturaBreakersA3;
+    // const alturaTotalBreakers = alturaBreakersA1 + alturaBreakersA2 + alturaBreakersA3;
 
     // Calcular la altura adicional según el amperaje
     let alturaAmperaje = 0;
@@ -48,12 +56,24 @@ function calcularAltura() {
         alturaAmperaje = 44;
     }
 
-    // Sumar la altura de MLO si se incluye (nuevo valor de 5.5 pulgadas)
-    let alturaMLO = incluirMLO ? 5.5 : 0;
 
-    // Calcular altura total del panel
-    const alturaTotal = alturaTotalBreakers + alturaAmperaje + alturaMLO + 3; // 3 es la distancia de SR (espacio de seguridad)
+     // Cálculo de la altura total
+     let alturaTotal = Math.ceil(breakerA1 / 2) * 3 + Math.ceil(breakerA2 / 2) * 4.125 + Math.ceil(breakerA3 / 2) * 5.5 + alturaAmperaje + 3; // A1 + A2 + + A3 + Amp + SR
+ 
+     // Si se incluye MLO, se suma 5.5 pulgadas
+     if (incluirMLO) {
+         alturaTotal += 5.5;
+     }
 
-    // Mostrar el resultado
-    document.getElementById("resultado").innerText = `Altura total del panel: ${alturaTotal} pulgadas`;
+     // Si se incluye MCB, se obtiene el tamaño seleccionado y se suma
+     if (incluirMCB) {
+         mcbTamaño = parseFloat(document.getElementById("mcbSize").value);
+         alturaTotal += mcbTamaño;
+     }
+
+     document.getElementById("resultado").innerHTML = `<h3>La altura total del panel es: ${alturaTotal} pulgadas</h3>`;
+}
+
+function volverMenu() {
+    window.location.href = 'index.html';  // Redirige al menú principal
 }
